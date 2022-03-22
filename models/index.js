c19196b9bfe399314257a6068f5afbcdb3fb3cec
Andrew An <andyan7@gmail.com>
2022-03-21 21:02:44 -0700

    adjusted index.js

--- models/index.js
+++ models/index.js
@@ -16,13 +16,15 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-});
+  through: {
+    model: ProductTag,
+}});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-});
+  through: {
+    model: ProductTag,
+}});
 
 module.exports = {
   Product,
